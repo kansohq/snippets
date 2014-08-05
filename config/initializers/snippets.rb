c2521b1fddadf0906670e2cbb190d86ed76b02f8
Robert May <rob@afternoonrobot.co.uk>
2014-08-05 11:15:54 +0100

    Remove redis connection errors on initialize

--- config/initializers/snippets.rb
+++ config/initializers/snippets.rb
@@ -2,19 +2,11 @@ require 'redis'
 
 Rails.application.config.after_initialize do
   begin
-    Redis.current.ping
-
     I18n.backend = I18n::Backend::Chain.new(
       I18n::Backend::KeyValue.new(Redis.current),
       I18n.backend
     )
 
     Snippets::Snippet.cache_all if Snippets::Snippet.table_exists?
-
-  rescue Redis::CannotConnectError => ex
-    unless Rails.env.development? || Rails.env.test? ||
-           (ENV['REDIS_URL'].nil? && ENV['REDIS_PROVIDER'].nil?)
-      raise ex
-    end
   end
 end
